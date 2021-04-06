2ebe52da5b20f637980aef32d3d083fbd528e1ec
Z440 <elena.zatykina@gmail.com>
2021-04-06 19:17:09 +0200

    login_link locators added

--- pages/locators.py
+++ pages/locators.py
@@ -18,3 +18,6 @@ class ProductBasketPageLocators():
     PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, ".col-sm-4 >h3>a")
     PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, ".col-sm-1 >p")
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
