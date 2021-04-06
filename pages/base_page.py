a53581d7508f8d3e69285946bdb9af06c13a9af9
Z440 <elena.zatykina@gmail.com>
2021-04-06 18:05:59 +0200

    element present and not present added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,16 @@
 from selenium.common.exceptions import NoSuchElementException
 import math
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10) -> object:
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -17,6 +21,25 @@ class BasePage:
         except (NoSuchElementException):
             return False
         return True
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
