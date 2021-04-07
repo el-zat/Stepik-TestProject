1748315e5827a528f3e476dd8304364bac1bf447
Z440 <elena.zatykina@gmail.com>
2021-04-07 15:35:18 +0200

    tests authorized_user and login_link added

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,11 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from TestProject.pages.locators import BasePageLocators
 
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -60,3 +60,7 @@ class BasePage:
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
