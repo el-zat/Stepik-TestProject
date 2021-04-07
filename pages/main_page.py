849a22388bf3f6dc813feaa2680f55664ee96596
Z440 <elena.zatykina@gmail.com>
2021-04-07 15:43:28 +0200

    all methods deleted

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,8 @@
 from TestProject.pages.base_page import BasePage
-from TestProject.pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
+
+
