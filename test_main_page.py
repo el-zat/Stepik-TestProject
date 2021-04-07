f1e772c1cb31836778c1d945e6c9e1a88cdf631d
Z440 <elena.zatykina@gmail.com>
2021-04-07 15:22:08 +0200

    added class TestLoginMainPage and makks need_review

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,53 @@
-from TestProject.pages.main_page import MainPage
+import pytest
 from TestProject.pages.login_page import LoginPage
+from TestProject.pages.basket_page import BasketPage
+from TestProject.pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()  # открываем страницу
-    page.go_to_login_page()
+@pytest.mark.login_guest
+class TestLoginMainPage:
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-def test_should_be_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()  # открываем страницу
-    link = page.go_to_login_page()
+
+def test_guest_can_see_login_form_in_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
-    page.should_be_login_page()
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_can_see_register_form_in_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/basket/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.cant_see_products_in_basket()
+    page.can_see_empty_basket()
+
+
+
+
+
+
+
+
+
+
+
+
